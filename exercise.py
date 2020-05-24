bf3f5e55c1454f2ad36840b19c815b41283ce193
Pedro Leoni <pedro.leoni@marconirovereto.it>
2020-05-24 18:19:55 +0200

    edges and gaussian blur

--- exercise.py
+++ exercise.py
@@ -11,6 +11,12 @@ cv2.imshow("immagine",img)
 grayimg = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 cv2.imshow("immagine scala di grigi",grayimg)
 
+edgesimg = cv2.Canny(img,50,150)
+cv2.imshow("immagine bordi", edgesimg)
+
+
+imgblur = cv2.GaussianBlur(img,(11,11),0)
+cv2.imshow("immagine filtro gaussian blur",imgblur)
 
 
 cv2.waitKey(0)
